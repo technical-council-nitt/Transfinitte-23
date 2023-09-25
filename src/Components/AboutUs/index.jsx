474500cfc8347b93d5963cceee002e13d4434b2e
Kavin Palanisamy <82392200+kavin-palanisamy@users.noreply.github.com>
2023-09-25 07:34:27 +0530

    landing (#3)
    * Delete src directory
    
    * Add files via upload

--- src/Components/AboutUs/index.jsx
+++ src/Components/AboutUs/index.jsx
@@ -1,14 +1,26 @@
-
-
+import "./styles.css";
 
 const AboutUs = () => {
-
-    return (
-        <>
-            <h1>About Us</h1>
-        </>
-    )
-}
-
+  return (
+    <div className="aboutPage">
+      <div className="inBox">
+        <div className="Box">
+          <div className="text-wrapper">About Us</div>
+          <p className="div">
+            The department lays prime focus on academics interspersed with
+            co-curricular and extra-curricular activities that bring the
+            versatility of its students to the fore and gives them a sound sense
+            of perspective. The faculty comprises of experienced and dedicated
+            teachers who with their expert inputs encourage students to explore
+            new avenues. The computer society “Websters” was started with the
+            aim to foster interest in the world of computers and technology. It
+            provides a platform for like-minded brains to communicate with each
+            other and expand their horizons.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AboutUs;
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,14 +1,28 @@
-
-import "./header.css"
+import "./header.css";
+import logo from "../../assets/logo.svg";
 
 const Header = () => {
-
-    return (
-        <>
-            <h1 className="header">Header</h1>
-        </>
-    )
-}
-
+  return (
+    <nav className="navbar">
+      <div className="navbarContent">
+        <img src={logo} alt="Logo" className="logo" />
+        <div className="navbarMenu">
+          <div>
+            <p>About Us</p>
+          </div>
+          <div>
+            <p>Timeline</p>
+          </div>
+          <div>
+            <p>Sponsors</p>
+          </div>
+          <div>
+            <p>Contact</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
