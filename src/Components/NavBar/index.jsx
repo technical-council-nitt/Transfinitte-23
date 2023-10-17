30b76c2059b419f777b441f9847c1c0c8f352089
kav1n <pkavin04@gmail.com>
2023-10-18 00:44:40 +0530

    add_sponsor_logo

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -1,168 +1,177 @@
 import { Text } from "@mantine/core";
+import "./Header.css";
+
 import logo from "../../assets/logo.svg";
+import tclogo from "../../assets/tc.svg";
+
 import {
-	Burger,
-	Container,
-	createStyles,
-	Group,
-	Header,
-	Paper,
-	Transition,
+  Burger,
+  Container,
+  createStyles,
+  Group,
+  Header,
+  Paper,
+  Transition,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useState } from "react";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import { useNavigate } from "react-router";
 
 const HEADER_HEIGHT = "3.5rem";
 
 const useStyles = createStyles((theme) => ({
-	root: {
-		position: "absolute",
-		zIndex: 1,
-		backgroundColor: "rgba(0,0,0,0)",
-		borderColor: "rgba(0,0,0,0)",
-	},
+  root: {
+    position: "absolute",
+    zIndex: 1,
+    backgroundColor: "rgba(0,0,0,0)",
+    borderColor: "rgba(0,0,0,0)",
+  },
 
-	dropdown: {
-		position: "absolute",
-		top: "4rem",
-		left: 0,
-		right: 0,
-		zIndex: 0,
-		borderTopRightRadius: 0,
-		borderTopLeftRadius: 0,
-		borderTopWidth: 0,
-		overflow: "hidden",
-		fontFamily: "Montserrat",
-		backgroundColor: "rgba(0,0,0,0.5)",
-		borderColor: "#ECECEC",
-		[theme.fn.largerThan("sm")]: {
-			display: "none",
-		},
-	},
+  dropdown: {
+    position: "absolute",
+    top: "4rem",
+    left: 0,
+    right: 0,
+    zIndex: 0,
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
+    borderTopWidth: 0,
+    overflow: "hidden",
+    fontFamily: "Montserrat",
+    backgroundColor: "rgba(0,0,0,0.5)",
+    borderColor: "#ECECEC",
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
 
-	header: {
-		display: "flex",
-		justifyContent: "space-between",
-		alignItems: "center",
-		height: "100%",
-		padding: "1rem 2rem",
-	},
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "100%",
+    padding: "1rem 2rem",
+  },
 
-	links: {
-		[theme.fn.smallerThan("sm")]: {
-			display: "none",
-		},
-	},
+  links: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
 
-	burger: {
-		backgroundColor: "none",
-		border: "1px #ECECEC solid",
-		[theme.fn.largerThan("sm")]: {
-			display: "none",
-		},
-	},
+  burger: {
+    backgroundColor: "none",
+    border: "1px #ECECEC solid",
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
 
-	link: {
-		display: "block",
-		lineHeight: 1,
-		cursor: "pointer",
-		padding: "0.5rem 0.75rem",
-		textDecoration: "none",
-		borderRadius: 0,
-		color: "#ECECEC",
-		fontSize: theme.fontSizes.sm,
-		fontWeight: 500,
-		transition: "0.3s",
-		"&:hover": {
-			color: "white",
-			backgroundColor: "rgba(0,0,0,0.3)",
-		},
+  link: {
+    display: "block",
+    lineHeight: 1,
+    cursor: "pointer",
+    padding: "0.5rem 0.75rem",
+    textDecoration: "none",
+    borderRadius: 0,
+    color: "#ECECEC",
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+    transition: "0.3s",
+    "&:hover": {
+      color: "white",
+      backgroundColor: "rgba(0,0,0,0.3)",
+    },
 
-		[theme.fn.smallerThan("sm")]: {
-			borderRadius: 0,
-			padding: theme.spacing.md,
-		},
-	},
-	blue: {
-		color: "cyan",
-	},
+    [theme.fn.smallerThan("sm")]: {
+      borderRadius: 0,
+      padding: theme.spacing.md,
+    },
+  },
+  blue: {
+    color: "cyan",
+  },
 }));
 
 const NavBar = () => {
-	const links = [{'label' : "About US", url : "#aboutus"},
-	{'label' : "Contact", url : "#contact"},
-	{'label' : "Sponsors", url : "#sponsors"},
-	{'label' : "Timeline", url : "#timeline"},
-	{'label' : "FaQs", url : "#faqs"},
-	{'label' : "Judges", url : "#judges"}];
+  const links = [
+    { label: "About US", url: "#aboutus" },
+    { label: "Contact", url: "#contact" },
+    { label: "Sponsors", url: "#sponsors" },
+    { label: "Timeline", url: "#timeline" },
+    { label: "FaQs", url: "#faqs" },
+    { label: "Judges", url: "#judges" },
+  ];
 
-	const [opened, { toggle, close }] = useDisclosure(false);
-	const [active, setActive] = useState(links[0].link);
-	const { classes, cx } = useStyles();
-	const navigate = useNavigate();
-	const items = links.map((link) => (
-		<a
-			key={link.label}
-			className={classes.link}
-			href={link.url}
-		>
-			{link.label}
-		</a>
-	));
-	return (
-		<Header height={HEADER_HEIGHT} className={classes.root}>
-			<Container className={classes.header} fluid>
-				<Text
-					sx={(theme) => ({
-						fontFamily: "Montserrat",
-						color: "white",
-						fontSize: theme.fontSizes.xl,
-						fontWeight: 700,
-						zIndex: 100,
-						"@media (max-width: 900px)": {
-							fontSize: theme.fontSizes.lg,
-							margin: "0rem",
-						},
-					})}
-				>
-					<motion.img src={logo} drag dragConstraints = {{left : 0, right : 0, top : 0, bottom : 0}} dragElastic = {1} transition = {{duration : 0.5}}></motion.img>
-				</Text>
-				<Group
-					spacing={2}
-					className={classes.links}
-					sx={{ fontFamily: "Montserrat" }}
-				>
-					{items}
-				</Group>
+  const [opened, { toggle, close }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
+  const { classes, cx } = useStyles();
+  const navigate = useNavigate();
+  const items = links.map((link) => (
+    <a key={link.label} className={classes.link} href={link.url}>
+      {link.label}
+    </a>
+  ));
+  return (
+    <Header height={HEADER_HEIGHT} className={classes.root}>
+      <Container className={classes.header} fluid>
+        <Text
+          sx={(theme) => ({
+            fontFamily: "Montserrat",
+            color: "white",
+            fontSize: theme.fontSizes.xl,
+            fontWeight: 700,
+            zIndex: 100,
+            "@media (max-width: 900px)": {
+              fontSize: theme.fontSizes.lg,
+              margin: "0rem",
+            },
+          })}
+        >
+          <motion.img
+            src={logo}
+            id="logo"
+            drag
+            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+            dragElastic={1}
+            transition={{ duration: 0.5 }}
+          ></motion.img>
+          <motion.img
+            src={tclogo}
+            id="tclogo"
+            drag
+            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+            dragElastic={1}
+            transition={{ duration: 0.5 }}
+          ></motion.img>
+        </Text>
+        <Group
+          spacing={2}
+          className={classes.links}
+          sx={{ fontFamily: "Montserrat" }}
+        >
+          {items}
+        </Group>
 
-				<Burger
-					opened={opened}
-					onClick={toggle}
-					className={classes.burger}
-					size="sm"
-					color="#ECECEC"
-				/>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          className={classes.burger}
+          size="sm"
+          color="#ECECEC"
+        />
 
-				<Transition
-					transition="pop-top-right"
-					duration={200}
-					mounted={opened}
-				>
-					{(styles) => (
-						<Paper
-							className={classes.dropdown}
-							withBorder
-							style={styles}
-						>
-							{items}
-						</Paper>
-					)}
-				</Transition>
-			</Container>
-		</Header>
-	);
+        <Transition transition="pop-top-right" duration={200} mounted={opened}>
+          {(styles) => (
+            <Paper className={classes.dropdown} withBorder style={styles}>
+              {items}
+            </Paper>
+          )}
+        </Transition>
+      </Container>
+    </Header>
+  );
 };
 
 export default NavBar;
